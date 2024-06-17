d65556191a34fce79380308a10d3f464b535a73f
obillekyle <obillekyle@gmail.com>
2024-06-17 17:03:12 +0800

    fix scroller missing

--- src/components/Text/index.ts
+++ src/components/Text/index.ts
@@ -1,11 +1,19 @@
 import Text from './text.vue'
 import Floater from './floater.vue'
 import AwaitedText from './awaited-text.vue'
+import Scroller from './scroller.vue'
 
 type TextComponent = typeof Text & {
   Floater: typeof Floater
   Awaited: typeof AwaitedText
+  Scroller: typeof Scroller
 }
 
-export default Text as TextComponent
-export { Text, Floater, AwaitedText }
+const TextElem = Text as TextComponent
+
+Text.Floater = Floater
+Text.Awaited = AwaitedText
+Text.Scroller = Scroller
+
+export default TextElem
+export { Text, Floater, AwaitedText, Scroller }
