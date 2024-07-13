7301570043e872664159385101548ffd3ab53654
obillekyle <obillekyle@gmail.com>
2024-07-13 22:57:39 +0800

    refactor(elements): move button-group and chip-group styles to button.css

--- packages/lib/src/components/Button/button-group.vue
+++ packages/lib/src/components/Button/button-group.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+  import '@/assets/button.scss'
+
   defineProps<{
     span?: boolean
   }>()
@@ -9,44 +11,3 @@
     <slot />
   </div>
 </template>
-
-<style lang="scss">
-  .md-button-group {
-    display: flex;
-    flex-wrap: nowrap;
-
-    > *:not(.md-button) {
-      display: none;
-    }
-
-    .md-button {
-      margin: 0;
-      border-radius: 0;
-      margin-inline: 0 !important;
-
-      &:not(:last-child) {
-        border-right: none;
-      }
-
-      &:first-child {
-        border-top-left-radius: var(--radius);
-        border-bottom-left-radius: var(--radius);
-      }
-
-      &:last-child {
-        border-top-right-radius: var(--radius);
-        border-bottom-right-radius: var(--radius);
-      }
-    }
-
-    &.span {
-      display: grid;
-      width: 100%;
-      grid-template-columns: repeat(auto-fit, minmax(0, 1fr));
-
-      .md-button {
-        width: 100%;
-      }
-    }
-  }
-</style>
--- packages/lib/src/components/Chip/chip-group.vue
+++ packages/lib/src/components/Chip/chip-group.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+  import '@/assets/button.scss'
+
   defineProps<{
     span?: boolean
   }>()
@@ -9,43 +11,3 @@
     <slot />
   </div>
 </template>
-
-<style lang="scss">
-  .md-chip-group {
-    display: flex;
-    flex-wrap: nowrap;
-
-    > *:not(.md-chip) {
-      display: none;
-    }
-
-    .md-chip {
-      border-radius: 0;
-      margin: 0;
-
-      &:not(:last-child) {
-        border-right: none;
-      }
-
-      &:first-child {
-        border-top-left-radius: var(--radius);
-        border-bottom-left-radius: var(--radius);
-      }
-
-      &:last-child {
-        border-top-right-radius: var(--radius);
-        border-bottom-right-radius: var(--radius);
-      }
-    }
-
-    &.span {
-      display: grid;
-      width: 100%;
-      grid-template-columns: repeat(auto-fit, minmax(0, 1fr));
-
-      .md-chip {
-        width: 100%;
-      }
-    }
-  }
-</style>
