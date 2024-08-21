fccca0b78842ce46056be26db8aad81c153f4080
obillekyle <obillekyle@gmail.com>
2024-08-21 13:59:57 +0800

    refactor(core): add modal utils

---
+++ packages/lib/src/components/Modal/modal.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+  import Box from '../Box/box.vue'
+
+  defineOptions({ name: 'MdModal' })
+</script>
+
+<template>
+  <Box class="md-modal">
+    <slot />
+  </Box>
+</template>
---
+++ packages/lib/src/components/Modal/util.ts
@@ -0,0 +1,51 @@
+import type { UtilityFunction } from '@/utils/component-manager'
+import type { Component } from 'vue'
+
+type ModalActionOnClick = (utils: UtilityFunction<ModalProps>) => void
+
+type ModalAction = {
+  label: string
+  onClick: ModalActionOnClick
+  variant: 'filled' | 'elevated' | 'tonal' | 'outlined' | 'text'
+}
+
+export interface ModalProps {
+  icon?: string
+  title?: string
+  content: Component | string
+  actions?: ModalAction[]
+  closeable?: boolean
+  focusLock?: boolean
+}
+
+const defaultUtility: UtilityFunction<ModalProps> = {
+  id: Number.NaN,
+  close: () => {},
+  modify: () => {}
+}
+
+export const MODAL = {
+  DEFAULT_UTILITY: defaultUtility,
+
+  PRESET_ACTION_CLOSE: [
+    {
+      label: 'Close',
+      onClick: (utils) => utils.close()
+    }
+  ] as ModalAction[],
+
+  PRESET_ACTION_CONFIRM: function (confirmation: ModalActionOnClick) {
+    return [
+      {
+        label: 'Cancel',
+        variant: 'outlined',
+        onClick: (utils) => utils.close()
+      },
+      {
+        label: 'Confirm',
+        variant: 'filled',
+        onClick: confirmation
+      }
+    ] as ModalAction[]
+  }
+}
