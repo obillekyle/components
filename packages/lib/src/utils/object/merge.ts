e4358183e3092ba3226e07834fcfea80498f8d9e
obillekyle <obillekyle@gmail.com>
2024-08-23 15:21:09 +0800

    feat(utils/object): add `mergeObject` function
    Use mergeObject function to deeply merge the property
    of two objects. Can only merge pure objects

---
+++ packages/lib/src/utils/object/merge.ts
@@ -0,0 +1,22 @@
+import { isPureObject } from './is'
+
+export function mergeObject<T extends object, U extends object>(
+  target: T,
+  source: U
+): T & U {
+  const output = { ...target } as Record<string, any>
+
+  for (const key of Object.keys(source)) {
+    const targetValue = (target as any)[key]
+    const sourceValue = (source as any)[key]
+
+    if (sourceValue === undefined) continue
+
+    output[key] =
+      isPureObject(targetValue) && isPureObject(sourceValue)
+        ? mergeObject(targetValue, sourceValue)
+        : sourceValue
+  }
+
+  return output as T & U
+}
