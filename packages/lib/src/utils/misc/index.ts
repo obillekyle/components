b57f3f05f193e846add11940d4b9133719b4d432
obillekyle <obillekyle@gmail.com>
2024-09-05 20:31:24 +0800

    refactor(utils/misc): use map for temporary cache

--- packages/lib/src/utils/misc/index.ts
+++ packages/lib/src/utils/misc/index.ts
@@ -2,30 +2,29 @@ const langCodes: Record<string, string> = {
   ph: 'tl'
 }
 
-const translationStore: Record<string, Record<string, undefined>> = {}
+const store: Map<string, string> = new Map()
 
 export async function getTranslatedText(text: string, lang: string = 'en') {
   lang = langCodes[lang] || lang
+  const key = `${lang}:${text}`
 
-  if (!translationStore[lang]) translationStore[lang] = {}
-  if (translationStore[lang] && translationStore[lang][text]) {
-    return translationStore[lang][text]
-  }
+  if (store.has(key)) return store.get(key)!
 
   try {
-    const data = await fetch(
+    const req = await fetch(
       `https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=${lang}&dt=t&q=${text}`
     )
-    const json = await data.json()
-    translationStore[lang][text] = json[0][0][0]
-    return json[0][0][0]
+
+    if (!req.ok) throw new Error('Failed to get translation')
+
+    const json = await req.json()
+    const data = json[0][0][0] as string
+
+    store.set(key, data)
+    return data
   } catch {
     console.error(
-      'Failed to get translation for "' +
-        text +
-        '" in "' +
-        lang +
-        '" language'
+      `Failed to get translation for "${text}" in "${lang}" language`
     )
   }
 
