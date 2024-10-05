d34fb93de278f1bf4750199ef935fb94a6b9d6e4
obillekyle <obillekyle@gmail.com>
2024-10-05 22:55:54 +0800

    refactor(core/flex): separate flex css styles from box

--- packages/lib/src/components/Box/Flex/flex-box.vue
+++ packages/lib/src/components/Box/Flex/flex-box.vue
@@ -2,11 +2,11 @@
   import type { BoxProps } from '@/components/Box/util'
   import type { SizesString } from '@/utils/css/type'
 
-  import { getBoxProps } from '../util'
+  import { createStyle } from '@/utils/css/create-style'
 
   import Box from '../box.vue'
 
-  interface FlexBoxProps extends BoxProps {
+  interface FlexBoxProps extends /* @vue-ignore */ BoxProps {
     direction?: 'row' | 'column' | 'row-reverse' | 'column-reverse'
     align?: 'start' | 'center' | 'end' | (string & {})
     justify?: 'start' | 'center' | 'end' | (string & {})
@@ -16,22 +16,18 @@
 
   defineOptions({ name: 'MdFlexBox' })
   const props = defineProps<FlexBoxProps>()
-  const boxProps = getBoxProps(props)
+
+  const flexClass = createStyle(() => ({
+    flexDirection: props.direction,
+    justifyContent: props.justify,
+    alignItems: props.align,
+    flexWrap: props.wrap,
+    gap: props.gap
+  }))
 </script>
 
 <template>
-  <Box
-    class="md-flex"
-    v-bind="boxProps"
-    :styled="{
-      flexDirection: props.direction,
-      justifyContent: props.justify,
-      alignItems: props.align,
-      flexWrap: props.wrap,
-      gap: props.gap,
-      ...styled
-    }"
-  >
+  <Box :class="['md-flex', flexClass]">
     <slot />
   </Box>
 </template>
