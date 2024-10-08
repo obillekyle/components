aaf6203b400f4bafe6afe659f38578ed2bde8b5a
obillekyle <obillekyle@gmail.com>
2024-10-08 17:06:09 +0800

    feat(core/misc): add `overlay-provider` component
    Include all the overlay components in a single component

--- packages/lib/src/components/Misc/index.ts
+++ packages/lib/src/components/Misc/index.ts
@@ -1,4 +1,5 @@
 export { default as ColorBlock } from './color-block.vue'
 export { default as HybridComponent } from './hybrid-component.vue'
 export { default as HybridIcon } from './hybrid-icon.vue'
+export { default as OverlayProvider } from './overlay-provider.vue'
 export { default as ViewObserver } from './view-observer.vue'
