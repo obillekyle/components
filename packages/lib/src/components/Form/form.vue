ce661b8b5f2e7696b428d4edb16ad2dd7d01e4d1
obillekyle <obillekyle@gmail.com>
2024-10-24 21:15:08 +0800

    refactor(ref/use-form-value): reduce code repetition

--- packages/lib/src/components/Form/form.vue
+++ packages/lib/src/components/Form/form.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
+  import { toProxy } from '@/ref/tools'
   import { provide } from 'vue'
 
   defineOptions({ name: 'MdForm' })
   const model = defineModel<any>({ default: {} })
-  provide('md-form-value', model)
+  provide('md-form-value', toProxy(model))
 </script>
 
 <template>
