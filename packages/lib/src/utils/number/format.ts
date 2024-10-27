9bae14383b49dac977528a69a82a65df4b7f6c91
obillekyle <obillekyle@gmail.com>
2024-10-27 16:42:45 +0800

    refactor(utils/number): `format`: use Math.round instead of value.toFixed

--- packages/lib/src/utils/number/format.ts
+++ packages/lib/src/utils/number/format.ts
@@ -2,6 +2,7 @@ export function trimDecimalZeros(value: string): string {
   return value.replace(/(\.\d*?)0+$/, '$1').replace(/\.$/, '')
 }
 
-export function toDecimalFixed(value: number, digits: number) {
-  return Number.parseFloat(value.toFixed(digits))
+export function toDecimalFixed(value: number, digits = 0) {
+  const factor = Math.pow(10, digits)
+  return Math.round(value * factor) / factor
 }
